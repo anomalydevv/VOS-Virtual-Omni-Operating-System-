84e8d0e45ebad805df547c6b89a4159ae2600e20
iskender <anomalydevv@gmail.com>
2026-08-27 23:36:01 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+
+import sys
+import os 
+os.system('cls' if os.name == 'nt' else 'clear')
+try:
+    
+    from src.Kernel.shock import call
+    from src.Kernel.entry import Main
+    
+    Main()
+
+except BaseException as e:
+    
+    try:
+        from src.Kernel.shock import call
+        call(e)
+        sys.exit(int(abs(hash(type(e).__name__)) % 100000))
+    except Exception:
+        print(f"[CKS] {e}")
+        sys.exit(-233)
